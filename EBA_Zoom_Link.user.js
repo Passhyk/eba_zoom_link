c78e181e6e3ba5830b81fdf94f72e90f03723936
bytescreator <ata2014kara2@gmail.com>
2020-11-24 19:03:05 +0300

    Bump Version (user.js)

--- EBA_Zoom_Link.user.js
+++ EBA_Zoom_Link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         EBA~Zoom Link
-// @version      0.2.9
+// @version      0.3.0
 // @namespace    https://ders.eba.gov.tr/
 // @description  EBA canlı derslerine Zoom uygulaması üzerinden ulaşın!
 // @author       Çağlar Turalı
